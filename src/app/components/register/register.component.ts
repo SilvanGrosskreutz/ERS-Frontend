92120787fecede30efcb0e005753107c62fc42f4
SilvanGrosskreutz <silvan.grosskreutz@htp-tel.de>
2022-07-07 15:56:15 +0200

    User Registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  username:string = "";
+  password:string = "";
+
+  role:string = "";
+
+  errorMsg:boolean = false;
+  notAllFieldFilled:boolean = false;
+  successMsg:boolean = false;
+
+  constructor(private userService:UserService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    this.notAllFieldFilled = false;
+    this.successMsg = false;
+    let user:User = new User(0, this.username, this.password, this.role);
+    if(this.username && this.password && this.role){
+      this.userService.register(user).subscribe({
+        next:()=>{
+          this.successMsg = true;
+        }, error:()=>{
+          this.errorMsg = true;
+        }
+      });
+    } else{
+      this.notAllFieldFilled = true;
+    }
+  }
 }
